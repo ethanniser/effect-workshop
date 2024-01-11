e5b8788f15e822566cebf6b6d8ac31cfb4e92f3f
Ethan Niser <100045248+ethanniser@users.noreply.github.com>
2024-01-11 21:02:39 +0000

    idek

--- example-apps/cli/index.ts
+++ example-apps/cli/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello via Bun!");
+import { Effect, Layer, pipe } from "effect";
+import { BunContext, KeyValueStore, Runtime } from "@effect/platform-bun";
+import { Command } from "@effect/cli";
+
+const bar = Command.make("bar", {}, () => Effect.log("bar"));
+
+const run = Command.make("foo", {}, () => Effect.log("hi")).pipe(
+  Command.withDescription("a test cli"),
+  Command.withSubcommands([bar]),
+  Command.run({
+    name: "Test",
+    version: "1.0.0",
+  })
+);
+
+const main = Effect.suspend(() => run(globalThis.process.argv.slice(2)));
+
+main.pipe(
+  Effect.provide(BunContext.layer),
+  Effect.tapErrorCause(Effect.logError),
+  Runtime.runMain
+);
