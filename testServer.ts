3a587c2ed8d9f2fc2ccbe9549961fd34c0409269
Ethan Niser <whatplan00@gmail.com>
2024-01-24 21:30:45 -0600

    add examples

--- testServer.ts
+++ testServer.ts
@@ -3,7 +3,7 @@ import { Hono } from "hono";
 const wait = (ms: number) => new Promise((r) => setTimeout(r, ms));
 
 const app = new Hono();
-app.post("/", async (c) => {
+app.post("/foo", async (c) => {
   const name = c.req.header("name") ?? "World";
   const raw = await c.req.json();
   const age = "age" in raw ? raw.age : "unknown";
@@ -15,3 +15,9 @@ export default {
   port: 3000,
   fetch: app.fetch,
 };
+
+/*
+examples:
+bun run index.ts -X POST -H "name: Ethan" --data "{ \"age\": 18 }" http://localhost:3000/foo 
+BEND_BASE_URL="http://localhost:3000" bun run index.ts -X POST -H "name: Ethan" --data "{ \"age\": 18 }" /foo
+*/
