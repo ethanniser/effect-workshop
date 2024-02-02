10890b7e249328163288e0d1664dc6ed99ea6932
Ethan Niser <100045248+ethanniser@users.noreply.github.com>
2024-02-02 16:26:27 +0000

    init part 6

---
+++ src/part2-cli/breakpoints/6-effect-platform/index.ts
@@ -0,0 +1,157 @@
+import { Console, Effect, Layer, Option, pipe } from "effect";
+import * as M from "./model";
+import * as S from "@effect/schema/Schema";
+
+const StringPairsFromStrings = S.array(S.string).pipe(
+  S.filter((arr) => arr.every((s) => s.split(": ").length === 2)),
+  S.transform(
+    S.array(S.tuple(S.string, S.string)),
+    (arr) =>
+      arr.map((s) => s.split(": ") as unknown as readonly [string, string]),
+    (arr) => arr.map((s) => s.join(": "))
+  )
+);
+
+function getCliOption(
+  cliArgs: string[],
+  option: { name: string; alias?: string }
+): Option.Option<string> {
+  return Option.gen(function* (_) {
+    const index = yield* _(
+      cliArgs.findIndex(
+        (arg) => arg === `--${option.name}` || arg === `-${option.alias}`
+      ),
+      (_) => (_ === -1 ? Option.none() : Option.some(_))
+    );
+    const nextIndex = index + 1;
+    const value = yield* _(Option.fromNullable(cliArgs[nextIndex]));
+    return value;
+  });
+}
+
+function getCliOptionMultiple(
+  cliArgs: string[],
+  option: { name: string; alias?: string }
+): string[] {
+  const indexes = cliArgs.reduce((acc, arg, index) => {
+    if (arg === `--${option.name}` || arg === `-${option.alias}`) {
+      if (index > cliArgs.length - 1) {
+        return acc;
+      }
+      acc.push(index);
+    }
+    return acc;
+  }, [] as number[]);
+  return indexes.reduce((acc, index) => {
+    acc.push(cliArgs[index + 1]!);
+    return acc;
+  }, [] as string[]);
+}
+
+const CliOptionsLive = Layer.effect(
+  M.CLIOptions,
+  Effect.gen(function* (_) {
+    const args = yield* _(Effect.sync(() => process.argv.slice(2)));
+
+    const method = getCliOption(args, { name: "method" }).pipe(
+      Option.getOrElse(() => "GET")
+    );
+    const data = getCliOption(args, { name: "data" });
+
+    const headers = yield* _(
+      getCliOptionMultiple(args, { name: "headers" }),
+      S.decode(StringPairsFromStrings),
+      Effect.mapError(() => new M.HeaderParseError())
+    );
+
+    const output = getCliOption(args, { name: "output" });
+    const include = getCliOption(args, { name: "include" }).pipe(
+      Option.flatMap((_) =>
+        ["true", "false"].includes(_) ? Option.some(_) : Option.none()
+      ),
+      Option.map((_) => _ === "true")
+    );
+
+    const url = yield* _(
+      Option.fromNullable(null),
+      Effect.mapError(
+        () => new M.CliOptionsParseError({ error: "No url provided" })
+      )
+    );
+
+    return {
+      url,
+      method,
+      data,
+      headers,
+      output,
+      include,
+    };
+  })
+);
+
+const main = Effect.gen(function* (_) {
+  const options = yield* _(M.CLIOptions);
+
+  const providedFetch = yield* _(M.Fetch);
+
+  const body = Option.getOrUndefined(options.data);
+
+  const res = yield* _(
+    Effect.tryPromise({
+      try: (signal) =>
+        providedFetch(options.url, {
+          ...(body && { body }),
+          method: options.method,
+          headers: Object.fromEntries(options.headers),
+          signal,
+        }),
+      catch: (error) => new M.UnknownError({ error }),
+    })
+  );
+
+  const buffer: string[] = [];
+
+  if (options?.include) {
+    buffer.push(`${res.status} ${res.statusText}`);
+    res.headers.forEach((value, key) => {
+      buffer.push(`${key}: ${value}`);
+    });
+    // Add an empty line to separate headers from body
+    buffer.push("");
+  }
+
+  const text = yield* _(
+    Effect.tryPromise({
+      try: () => res.text(),
+      catch: () => new M.TextDecodeError(),
+    })
+  );
+  buffer.push(text);
+
+  const finalString = buffer.join("\n");
+  yield* _(
+    Effect.match(options.output, {
+      onSuccess: (output) => Effect.sync(() => Bun.write(output, finalString)),
+      onFailure: () => Console.log(finalString),
+    })
+  );
+});
+
+await pipe(
+  main,
+  Effect.catchTags({
+    TextDecodeError: (error) => Console.error("Text decode error: ", error),
+    UnknownError: (error) => Console.error("Unknown error: ", error),
+  }),
+  Effect.provideService(M.Fetch, globalThis.fetch),
+  Effect.provide(CliOptionsLive),
+  Effect.match({
+    onSuccess: () => process.exit(0),
+    onFailure: (cause) => {
+      console.error(cause);
+      process.exit(1);
+    },
+  }),
+  Effect.runPromise
+);
---
+++ src/part2-cli/breakpoints/6-effect-platform/model.ts
@@ -0,0 +1,36 @@
+import { Context, Data, Option } from "effect";
+
+export class UnknownError extends Data.TaggedError("UnknownError")<{
+  readonly error: unknown;
+}> {}
+
+export class TextDecodeError extends Data.TaggedError("TextDecodeError") {}
+
+export class HeaderParseError extends Data.TaggedError("HeaderParseError") {}
+
+type Fetch = {
+  readonly _: unique symbol;
+};
+
+export const Fetch = Context.Tag<Fetch, typeof globalThis.fetch>("Fetch");
+
+type CLIOptions = {
+  readonly _: unique symbol;
+};
+
+interface CLIOptionsImpl {
+  readonly url: string;
+  readonly method: string;
+  readonly data: Option.Option<string>;
+  readonly headers: readonly (readonly [string, string])[];
+  readonly output: Option.Option<string>;
+  readonly include: Option.Option<boolean>;
+}
+
+export const CLIOptions = Context.Tag<CLIOptions, CLIOptionsImpl>("CLIOptions");
+
+export class CliOptionsParseError extends Data.TaggedError(
+  "CliOptionsParseError"
+)<{
+  readonly error: unknown;
+}> {}
