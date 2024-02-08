184f1fe4cbe7ee9075a8c33a90205d1831d2720b
Ethan Niser <whatplan00@gmail.com>
2024-02-07 18:07:50 -0600

    jank async await impl

--- src/part1-basics/optionGen.ts
+++ src/part1-basics/optionGen.ts
@@ -5,7 +5,7 @@ type OptionGenF<A> = () => Generator<Option.Option<A>, A, A>;
 
 function MyOptionGen<A>(generatorF: OptionGenF<A>): Option.Option<A> {
   const generator = generatorF();
-  let next: IteratorResult<Option.Option<A>, A> = generator.next();
+  let next = generator.next();
   while (true) {
     if (next.done) {
       return Option.some(next.value);
@@ -19,20 +19,73 @@ function MyOptionGen<A>(generatorF: OptionGenF<A>): Option.Option<A> {
   }
 }
 
-const A: Option.Option<number> = Option.some(1);
-function B(n: number): Option.Option<number> {
-  //   return Option.some(n + 1);
-  return Option.none();
-}
-function C(n: number): Option.Option<number> {
-  return Option.some(n + 2);
-}
+// const A: Option.Option<number> = Option.some(1);
+// function B(n: number): Option.Option<number> {
+//   return Option.some(n + 1);
+//   // return Option.none();
+// }
+// function C(n: number): Option.Option<number> {
+//   return Option.some(n + 2);
+// }
 
 const result = MyOptionGen(function* () {
-  const a = yield A;
-  const b = yield B(a);
-  const c = yield C(b);
-  return c;
+  const a = yield A; // 1
+  const b = yield B(a); // 2
+  const c = yield C(b); // 4
+  return c * 2; // 8
 } as OptionGenF<number>);
 
-console.log(result);
+console.log("option", result);
+
+type AsyncAwaitGen<A> = () => Generator<Promise<A>, A, A>;
+
+function MyAsyncAwait<A>(generatorF: AsyncAwaitGen<A>): Promise<A> {
+  const generator = generatorF();
+  return new Promise((resolve, reject) => {
+    const handleNext = (value: A) => {
+      const next = generator.next(value);
+      if (next.done) {
+        resolve(next.value);
+      } else {
+        next.value.then(handleNext).catch(reject);
+      }
+    };
+
+    const first = generator.next();
+    if (first.done) {
+      resolve(first.value);
+    } else {
+      first.value.then(handleNext).catch(reject);
+    }
+  });
+}
+
+function A(): Promise<number> {
+  return Promise.resolve(1);
+}
+
+function B(n: number): Promise<number> {
+  return Promise.resolve(n + 1);
+}
+
+function C(n: number): Promise<number> {
+  return Promise.resolve(n + 2);
+}
+
+MyAsyncAwait(function* () {
+  const a = yield A(); // 1
+  const b = yield B(a); // 2
+  const c = yield C(b); // 4
+  return c * 2; // 8
+} as AsyncAwaitGen<number>).then((result) => {
+  console.log("async await", result); // 8 !!
+});
+
+// const result2 = await MyAsyncAwait(function* () {
+//   const a = yield A2(); // 1
+//   const b = yield B2(a); // 2
+//   const c = yield C2(b); // 4
+//   return c * 2; // 8
+// } as AsyncAwaitGen<number>);
+
+// console.log("async await", result2); // 8 !!
