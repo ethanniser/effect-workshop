f528c1733e68630571e5e664d4b82f0c2b8a2425
Ethan Niser <whatplan00@gmail.com>
2024-02-10 18:43:27 -0600

    stm done

---
+++ src/part4-whatsnext/snippets/stm.ts
@@ -0,0 +1,165 @@
+import { Console, Effect, STM, TRef, pipe } from "effect";
+
+// Many times we want to share state between different parts of our application.
+// and its crucial that updates to this state are atomic and consistent.
+// The STM module provides a way to do this. Its a port of the Haskell STM
+// and features a api that is very similar to the one your used to in effect.
+// instead of a Effect<A, E, R> to represent a program with side effects,
+// we have a STM<A, E, R> to represent a transaction.
+// No side effects are allowed in a transaction, only pure computations and updates to state.
+
+// just like how creating an effect does not run it, creating a transaction does not run it.
+// only when we `commit` the transaction does it run
+// if any of the computations in the transaction fail, the transaction is rolled back and retried.
+
+// Think of STM like a completely seperate world from the rest of your program.
+// When you commit a transaction, even if it is made up of many different computations,
+// To the rest of your program it looks like a single atomic operation.
+
+// You can think of this like how the function passed to `Effect.sync` gets run
+// It is guaranteed to run until it completes or fails, in one single synchronous step.
+// STM is like writing in the effect style, with tracked errors and dependencies but with this same guarantee.
+
+// STM doesnt work on any state, but effect provides `T_` variants of many of its data types
+// the core of these is `TRef` which is a transactional reference to a value.
+
+// just like how get/set/update operations on a Ref return an Effect
+// the same operations on a TRef return an STM
+
+const program = Effect.gen(function* (_) {
+  const ref = yield* _(TRef.make(0));
+
+  const transaction = pipe(
+    TRef.get(ref),
+    STM.flatMap((i) => TRef.set(ref, i + 1))
+  );
+
+  const value = yield* _(STM.commit(TRef.get(ref)));
+  console.log(value); // 0
+
+  yield* _(STM.commit(transaction));
+  const value2 = yield* _(STM.commit(TRef.get(ref)));
+  console.log(value2); // 1
+});
+
+// Also the `commit` is actually optional,
+// STMs are a subtype of Effect and commit is basically used internally if you omit it
+
+// lets take a look at a more complex example
+
+// First out data model, an account with a balance that is stored in a TRef
+
+interface Account {
+  id: number;
+  balance: TRef.TRef<number>;
+}
+
+class InsufficientFundsError {
+  readonly _tag = "InsufficientFundsError";
+}
+
+const createAccount = (id: number, initialBalance: number): STM.STM<Account> =>
+  TRef.make(initialBalance).pipe(STM.map((balance) => ({ id, balance })));
+
+const transfer = (
+  from: Account,
+  to: Account,
+  amount: number
+): STM.STM<void, InsufficientFundsError> =>
+  STM.gen(function* (_) {
+    const fromBalance = yield* _(TRef.get(from.balance));
+    const toBalance = yield* _(TRef.get(to.balance));
+
+    if (fromBalance < amount) {
+      return yield* _(STM.fail(new InsufficientFundsError()));
+    }
+
+    yield* _(TRef.set(from.balance, fromBalance - amount));
+    yield* _(TRef.set(to.balance, toBalance + amount));
+  });
+
+const main = Effect.gen(function* (_) {
+  const account1 = yield* _(createAccount(1, 1000));
+  const account2 = yield* _(createAccount(2, 500));
+  const account3 = yield* _(createAccount(3, 200));
+
+  // Concurrent transfers
+  const transfer1 = STM.commit(transfer(account1, account2, 200));
+  const transfer2 = STM.commit(transfer(account2, account1, 300));
+
+  // Execute transfers concurrently
+  yield* _(Effect.all([transfer1, transfer2], { concurrency: "unbounded" }));
+
+  // Check final balances
+  const finalBalance1 = yield* _(STM.commit(TRef.get(account1.balance)));
+  const finalBalance2 = yield* _(STM.commit(TRef.get(account2.balance)));
+
+  yield* _(Effect.log(`Final Balance Account 1: ${finalBalance1}`));
+  yield* _(Effect.log(`Final Balance Account 2: ${finalBalance2}`));
+
+  const badTransaction = STM.all([
+    transfer(account1, account2, 1000), // this also wont go through because the next one fails
+    transfer(account3, account1, 300),
+    transfer(account1, account3, 400), // this wont go through because the first one will fail
+  ]);
+
+  yield* _(
+    STM.commit(badTransaction),
+    Effect.catchAll((error) => Console.error(error._tag))
+  );
+
+  const finalBalance3 = yield* _(STM.commit(TRef.get(account3.balance)));
+  yield* _(Effect.log(`Final Balance Account 3: ${finalBalance3}`)); // this is the original amount
+});
+
+// Effect.runPromise(main);
+
+// STM transactions usually aren't very big
+// They only need to be as large, as you need to ensure atomicity and consistency
+// Like is this example the 'badTransaction' should really be seperate transactions
+// because they are not related to each other, so theres no reason to make them atomic together
+
+// an example where multiple transactions are related is maybe we have an account with a minimum balance
+// and some unknown transactions. We want to apply the process but at no point can the account go under the minimum balance
+
+const example = Effect.gen(function* (_) {
+  const account1 = yield* _(createAccount(1, 1000));
+  const account2 = yield* _(createAccount(2, 500));
+  const account3 = yield* _(createAccount(3, 200));
+
+  const transfers = STM.all([
+    transfer(account1, account2, 1000),
+    transfer(account2, account3, 500),
+    transfer(account3, account1, 400),
+  ]);
+
+  // we want to ensure that account1 never goes under 700
+
+  const ensuringMinimumBalance = STM.gen(function* (_) {
+    yield* _(transfers); // run the transfers
+    const balance = yield* _(TRef.get(account1.balance));
+    if (balance < 700) {
+      return yield* _(STM.fail(new InsufficientFundsError()));
+    }
+  });
+
+  yield* _(
+    STM.commit(ensuringMinimumBalance),
+    Effect.catchAll((error) => Console.error(error._tag))
+  );
+});
+
+Effect.runPromise(example); // all transactions roll back because our final check fails
+
+// what guarantees does STM give us?
+// 1. Atomicity: Each transfer operation is atomic. If any part of the transfer fails (e.g., due to insufficient funds),
+// the whole transaction is rolled back, ensuring account balances are never left in an inconsistent state.
+// 2. Consistency: The STM ensures the consistency of the bank system by making sure all operations either
+// complete fully or have no effect, maintaining the invariant that the total amount of money in the system remains constant.
+// 3. Isolation: Each transfer operation is isolated from others.
+// Intermediate states during a transfer are not visible to other transactions, preventing any inconsistent reads.
+
+// STM is a powerful tool for managing shared state in a concurrent environment.
+// It enabled these benefits without locks, and is a powerful tool for managing shared state in a concurrent environment.
+// Because js is single threaded, we can be 100% sure when we commit a transaction,
+// it will run to completion without any other transactions running in between.
--- test.ts
+++ test.ts
@@ -1,12 +1,57 @@
-import { Console, Effect, pipe } from "effect";
+import { Console, Effect, Fiber, Ref, Schedule, pipe } from "effect";
 
-const f1 = (): Promise<string> => {
-  throw new Error("thrown");
-};
+const logRef = (ref: Ref.Ref<number>) =>
+  pipe(
+    Ref.get(ref),
+    Effect.flatMap((i) => Effect.log(i)),
+    Effect.repeat(Schedule.spaced(250)),
+    Effect.fork
+  );
 
-const f2 = (): Promise<string> => Promise.reject(new Error("rejected"));
+const one = Effect.gen(function* (_) {
+  let ref = yield* _(Ref.make(0));
+  yield* _(logRef(ref));
 
-// await Effect.runPromise(Effect.tryPromise(f1));
+  while (true) {
+    yield* _(Ref.update(ref, (i) => i + 1));
+  }
+});
 
-const message = await Effect.runPromiseExit(Effect.tryPromise(f2));
-console.log(message);
+const two = Effect.gen(function* (_) {
+  let ref = yield* _(Ref.make(0));
+  yield* _(logRef(ref));
+
+  yield* _(
+    Ref.update(ref, (i) => i + 1),
+    Effect.forever
+  );
+});
+
+const three = Effect.gen(function* (_) {
+  let ref = yield* _(Ref.make(0));
+  yield* _(logRef(ref));
+
+  while (true) {
+    yield* _(Ref.update(ref, (i) => i + 1));
+    yield* _(Effect.yieldNow());
+  }
+});
+
+const main = Effect.gen(function* (_) {
+  yield* _(Console.log(" --- ONE --- "));
+  const handle = yield* _(Effect.fork(one));
+  yield* _(Effect.sleep("2 seconds"));
+  yield* _(Fiber.interrupt(handle));
+
+  yield* _(Console.log(" --- TWO --- "));
+  const handle2 = yield* _(Effect.fork(two));
+  yield* _(Effect.sleep("2 seconds"));
+  yield* _(Fiber.interrupt(handle2));
+
+  yield* _(Console.log(" --- THREE --- "));
+  const handle3 = yield* _(Effect.fork(three));
+  yield* _(Effect.sleep("2 seconds"));
+  yield* _(Fiber.interrupt(handle3));
+});
+
+Effect.runPromise(main);
