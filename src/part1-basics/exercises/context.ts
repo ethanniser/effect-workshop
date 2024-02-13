cc8354070dbf8c160cb14dfc9ab72f2fbf9fe1e9
Ethan Niser <whatplan00@gmail.com>
2024-02-12 20:37:30 -0600

    context exercises

--- src/part1-basics/exercises/context.ts
+++ src/part1-basics/exercises/context.ts
@@ -1,5 +1,56 @@
 import { Effect, Context, Layer } from "effect";
+import * as T from "../../testDriver";
 
-class Foo extends Context.Tag("Foo")<Foo, { foo: string }>() {
-  static readonly Live = { foo: "foo" };
+class Foo extends Context.Tag("Foo")<Foo, { readonly bar: string }>() {
+  static readonly Live = Layer.succeed(Foo, { bar: "imFromContext!" });
 }
+
+// Exercise 1
+// `Tags` being a subtype of `Effect` is a bit too easy
+// Get the `Foo` service from context manually :)
+
+const test1 = Effect.gen(function* (_) {
+  const foo = { bar: "hint: look at Effect.context" };
+  return foo.bar;
+}).pipe(Effect.provide(Foo.Live));
+
+T.testRunAssert(test1, { success: "imFromContext!" });
+
+// Exercise 2
+
+class Random extends Context.Tag("Random")<
+  Random,
+  {
+    readonly nextInt: Effect.Effect<number>;
+    readonly nextBool: Effect.Effect<boolean>;
+    readonly nextIntBetween: (
+      min: number,
+      max: number
+    ) => Effect.Effect<number>;
+  }
+>() {
+  static readonly Mock = Layer.succeed(Random, {
+    nextInt: Effect.succeed(42),
+    nextBool: Effect.succeed(true),
+    nextIntBetween: (min, max) => Effect.succeed(min + max),
+  });
+}
+
+// Having to get the service, just to use a single property or function is a bit annoying
+// For convenience lets create Effects (or functions that return Effects) themselves already depend on the service
+
+declare const nextInt: Effect.Effect<number, never, Random>;
+declare const nextBool: Effect.Effect<boolean, never, Random>;
+declare const nextIntBetween: (
+  min: number,
+  max: number
+) => Effect.Effect<number, never, Random>;
+
+const test2 = Effect.gen(function* (_) {
+  const int = yield* _(nextInt);
+  const bool = yield* _(nextBool);
+  const intBetween = yield* _(nextIntBetween(10, 20));
+  return { int, bool, intBetween };
+}).pipe(Effect.provide(Random.Mock));
+
+T.testRunAssert(test2, { success: { int: 42, bool: true, intBetween: 30 } });
--- src/solutions-DONT-CHEAT/solutions.md
+++ src/solutions-DONT-CHEAT/solutions.md
@@ -7,6 +7,8 @@ const file = (fd: number) =>
   Effect.acquireRelease(MockFile.open(fd), (file) => file.close);
 ```
 
+Using `Effect.acquireRelease` we can create a resource with a finalizer attached to the scope of the returned effect
+
 ### Exercise 2
 
 ```ts
@@ -22,3 +24,48 @@ const test2 = Effect.gen(function* (_) {
   yield* _(Scope.close(scope2, Exit.unit));
 });
 ```
+
+`Scope.extend` mutates an existing scope to include all finalizers from a given effect. We use this to keep the two file finalizers seperate. Then, with `Scope.close` we manually close the scopes.
+
+## Context
+
+### Exercise 1
+
+```ts
+const test1 = Effect.gen(function* (_) {
+  const context = yield* _(Effect.context<Foo>());
+  const foo = Context.get(context, Foo);
+  return foo.bar;
+});
+```
+
+`Effect.context` returns the current context. Then `Context.get` is used to extract the value of the context.
+
+### Exercise 2
+
+#### Manual Solution
+
+```ts
+const nextInt = Random.pipe(Effect.andThen((_) => _.nextInt));
+const nextBool = Random.pipe(Effect.andThen((_) => _.nextBool));
+const nextIntBetween = (min: number, max: number) =>
+  Random.pipe(Effect.andThen((_) => _.nextIntBetween(min, max)));
+```
+
+#### Easy Solution
+
+```ts
+const {
+  constants: { nextInt, nextBool },
+  functions: { nextIntBetween },
+} = Effect.serviceMembers(Random);
+```
+
+_also_
+
+```ts
+const { nextInt, nextBool } = Effect.serviceConstants(Random);
+const { nextIntBetween } = Effect.serviceFunctions(Random);
+```
+
+`Effect.service*` functions provide a convenient way to access parts of a service without having to manually get the service first.
