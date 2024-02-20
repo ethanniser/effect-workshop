8120cf534fbb896271644c507a37eab3bb3bef79
Ethan Niser <whatplan00@gmail.com>
2024-02-20 08:33:44 -0600

    pad

--- src/pad.ts
+++ src/pad.ts
@@ -1,36 +1,23 @@
-import { Resolver, Rpc } from "@effect/rpc";
-import * as S from "@effect/schema/Schema";
-import { Console, Effect, Layer, Stream } from "effect";
-import { HttpRouter, HttpResolver } from "@effect/rpc-http";
-import * as HttpServer from "@effect/platform/HttpServer";
-import * as HttpClient from "@effect/platform/HttpClient";
-import { NodeHttpServer, NodeRuntime } from "@effect/platform-node";
-import { createServer } from "node:http";
-import {
-  GetTodoById,
-  GetTodos,
-  type Router,
-} from "./part4-whatsnext/snippets/6-rpc";
+import { Console, Effect, Layer, Schedule, pipe } from "effect";
 
-const client = HttpResolver.make<Router>(
-  HttpClient.client
-    .fetchOk()
-    .pipe(
-      HttpClient.client.mapRequest(
-        HttpClient.request.prependUrl("http://0.0.0.0:3000/rpc")
-      )
-    )
-).pipe(Resolver.toClient);
-
-// and now we can use the client with our rpc requests
-
-client(new GetTodos()).pipe(
-  Stream.runCollect,
-  Effect.flatMap(
-    Effect.forEach(({ id }) => client(new GetTodoById({ id })), {
-      batching: true,
-    })
-  ),
-  Effect.tap(Console.log),
-  Effect.runFork
+const layer1 = Layer.effectDiscard(
+  Effect.gen(function* (_) {
+    yield* _(
+      Effect.log("layer 1"),
+      Effect.repeat(Schedule.spaced("1 seconds"))
+    );
+  })
 );
+
+const layer2 = Layer.effectDiscard(
+  Effect.gen(function* (_) {
+    yield* _(
+      Effect.log("layer 2"),
+      Effect.repeat(Schedule.spaced("1 seconds"))
+    );
+  })
+);
+
+const main = pipe(Layer.merge(layer1, layer2), Layer.launch);
+
+Effect.runPromise(main);
